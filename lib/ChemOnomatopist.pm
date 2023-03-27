dce5270883e37f3a21473e03ada032a53a476568
Andrius Merkys <andrius.merkys@gmail.com>
2023-03-27 18:16:16 +0300

    Fixing problems with rule_greatest_number_of_side_chains_new().

--- lib/ChemOnomatopist.pm
+++ lib/ChemOnomatopist.pm
@@ -588,17 +588,17 @@ sub rule_greatest_number_of_side_chains_new
 {
     my( $tree, @path_parts ) = @_;
 
-    my @sorted_values = sort { $b <=> $a }
-                        uniq map { $_->number_of_branches } @path_parts;
+    my @sorted_values = sort uniq map { $_->number_of_branches }
+                                      @path_parts;
 
     my @path_parts_now;
     my $seen_groups = set();
-    for my $value (@sorted_values) {
+    for my $value (reverse @sorted_values) {
         last if $seen_groups->size >= 2;
         push @path_parts_now,
              grep { $_->number_of_branches == $value &&
                     !$seen_groups->has( $_->group ) } @path_parts;
-        $seen_groups = set( map { $_->group } @path_parts );
+        $seen_groups = set( map { $_->group } @path_parts_now );
     }
 
     return @path_parts_now;
