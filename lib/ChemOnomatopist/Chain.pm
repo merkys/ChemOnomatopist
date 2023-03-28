9a762c5f36ac838783c59c0ea93b4ce4320ce69d
Andrius Merkys <andrius.merkys@gmail.com>
2023-03-28 17:07:58 +0300

    Converting tabs to spaces.

--- lib/ChemOnomatopist/Chain.pm
+++ lib/ChemOnomatopist/Chain.pm
@@ -9,21 +9,21 @@ use warnings;
 use List::Util qw( sum0 );
 
 sub AUTOLOAD {
-	our $AUTOLOAD;
-	my $call = $AUTOLOAD;
-	$call =~ s/.*:://;
-	return if $call eq 'DESTROY';
-	if( $call =~ /^number_/ ) {
-		return sum0 map { $_->can( $call )->() } @{$_[0]->{halves}};
-	} else {
-		return;
-	}
+    our $AUTOLOAD;
+    my $call = $AUTOLOAD;
+    $call =~ s/.*:://;
+    return if $call eq 'DESTROY';
+    if( $call =~ /^number_/ ) {
+        return sum0 map { $_->can( $call )->() } @{$_[0]->{halves}};
+    } else {
+        return;
+    }
 }
 
 sub new
 {
-	my( $class, @halves ) = @_;
-	return bless $class, { halves => \@halves };
+    my( $class, @halves ) = @_;
+    return bless $class, { halves => \@halves };
 }
 
 1;
