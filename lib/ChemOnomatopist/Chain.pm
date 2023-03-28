359c35b97731be9459377c7354ce868e6d097833
Andrius Merkys <andrius.merkys@gmail.com>
2023-03-28 17:07:28 +0300

    Defining AUTOLOAD for ChemOnomatopist::Chain.

--- lib/ChemOnomatopist/Chain.pm
+++ lib/ChemOnomatopist/Chain.pm
@@ -6,6 +6,20 @@ use warnings;
 # ABSTRACT: Longest chain in a compound
 # VERSION
 
+use List::Util qw( sum0 );
+
+sub AUTOLOAD {
+	our $AUTOLOAD;
+	my $call = $AUTOLOAD;
+	$call =~ s/.*:://;
+	return if $call eq 'DESTROY';
+	if( $call =~ /^number_/ ) {
+		return sum0 map { $_->can( $call )->() } @{$_[0]->{halves}};
+	} else {
+		return;
+	}
+}
+
 sub new
 {
 	my( $class, @halves ) = @_;
