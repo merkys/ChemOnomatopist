df304aaf44fd81c9ac8324a0b65a79465a5d6101
Andrius Merkys <andrius.merkys@gmail.com>
2023-04-26 15:56:36 +0300

    Encoding the order of seniority of groups in ChemOnomatopist::Group.

--- lib/ChemOnomatopist/Group.pm
+++ lib/ChemOnomatopist/Group.pm
@@ -3,8 +3,39 @@ package ChemOnomatopist::Group;
 use strict;
 use warnings;
 
+use ChemOnomatopist::Group::Carbonyl;
+use ChemOnomatopist::Group::Carboxyl;
+use ChemOnomatopist::Group::Hydroxy;
+
 # ABSTRACT: Chemical group
 # VERSION
+
+# Order from BBv2 P-41
+our @order = (
+    # Radicals
+    # Radical anions
+    # Radical cations
+    # Anions
+    # Zwitterions
+    # Cations
+    # Acids
+    ChemOnomatopist::Group::Carboxyl::,
+    # Anhydrides
+    # Esters
+    # Acid halides and pseudohalides
+    # Amides
+    # Hydrazides
+    # Imides
+    # Nitriles
+    # Aldehydes and chalcogen analogues
+    ChemOnomatopist::Group::Carbonyl::,
+    ChemOnomatopist::Group::Hydroxy::,
+    # Hydroperoxides
+    # Amines
+    # Imines
+
+    # Classes denoted by the senior atom in heterane nomenclature
+);
 
 sub new
 {
