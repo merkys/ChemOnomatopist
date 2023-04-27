568eeb802a2fe158ea12ea5e9b032139e02da06e
Andrius Merkys <andrius.merkys@gmail.com>
2023-04-27 17:35:59 +0300

    Attempting to construct all the possible chains having senior substituents.

---
+++ lib/ChemOnomatopist/Chain/VertexArray.pm
@@ -0,0 +1,31 @@
+package ChemOnomatopist::Chain::VertexArray;
+
+use strict;
+use warnings;
+
+# ABSTRACT: Chain built upon an array of vertices
+# VERSION
+
+use parent ChemOnomatopist::ChainHalf::;
+
+use List::Util qw( sum0 );
+
+sub new
+{
+    my( $class, $graph, @vertices ) = @_;
+    return bless { graph => $graph, vertices => \@vertices }, $class;
+}
+
+# sub branch_positions() # TODO: Maybe need to add 1 to all returned positions?
+
+sub _disconnected_chain_graph()
+{
+    my( $self ) = @_;
+
+    my $graph = $self->{graph}->copy;
+    $graph->delete_path( $self->vertices );
+
+    return $graph;
+}
+
+1;
--- lib/ChemOnomatopist/Group/Hydroxy.pm
+++ lib/ChemOnomatopist/Group/Hydroxy.pm
@@ -8,7 +8,11 @@ use warnings;
 
 use parent ChemOnomatopist::Group::;
 
-use ChemOnomatopist::Util::Graph qw( graph_path_between_vertices );
+use ChemOnomatopist::Chain::VertexArray;
+use ChemOnomatopist::Util::Graph qw(
+    graph_longest_paths_from_vertex
+    graph_path_between_vertices
+);
 use Scalar::Util qw( blessed );
 use Set::Object;
 
@@ -44,6 +48,38 @@ sub get_name
                     $max_value = $value;
                 } elsif( $max_value == $value ) {
                     push @paths, \@path;
+                }
+            }
+        }
+
+        # Construct all chains having all possible extensions to both sides of the selected path
+        my %longest_paths;
+        my @chains;
+        for my $path (@paths) {
+            my $copy = $graph->copy;
+            $copy->delete_path( @$path );
+
+            my $A = shift @$path;
+            my $B = pop @$path;
+
+            if( !exists $longest_paths{$A} ) {
+                $longest_paths{$A} = [ graph_longest_paths_from_vertex( $copy, $A ) ];
+            }
+            if( !exists $longest_paths{$B} ) {
+                $longest_paths{$B} = [ graph_longest_paths_from_vertex( $copy, $B ) ];
+            }
+
+            for my $i (0..$#{$longest_paths{$A}}) {
+                for my $j (0..$#{$longest_paths{$B}}) {
+                    push @chains,
+                         ChemOnomatopist::Chain::VertexArray->new( $graph,
+                                                                   reverse( @{$longest_paths{$A}->[$i]} ),
+                                                                   @$path,
+                                                                   @{$longest_paths{$B}->[$j]} ),
+                         ChemOnomatopist::Chain::VertexArray->new( $graph,
+                                                                   reverse( @{$longest_paths{$B}->[$j]} ),
+                                                                   reverse( @$path ),
+                                                                   @{$longest_paths{$A}->[$j]} );
                 }
             }
         }
