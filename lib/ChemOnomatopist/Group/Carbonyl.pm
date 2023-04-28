4a1590f006be3b5b1f6da2c3792ed847538c696e
Andrius Merkys <andrius.merkys@gmail.com>
2023-04-28 12:51:30 +0300

    Removing dead code from ChemOnomatopist::Group::Carbonyl.

--- lib/ChemOnomatopist/Group/Carbonyl.pm
+++ lib/ChemOnomatopist/Group/Carbonyl.pm
@@ -12,17 +12,6 @@ use Scalar::Util qw( blessed );
 
 sub is_oxygen { return 1 }
 
-sub get_name
-{
-    my( $class, $graph ) = @_;
-
-    my( $ketone ) = grep { blessed( $_ ) && $_->isa( $class ) } $graph->vertices;
-    my $name = ChemOnomatopist::get_sidechain_name( $graph, $ketone );
-    $name =~ s/yl$//;
-    $name .= 'an-1-' unless $name =~ /-$/;
-    return $name . 'one';
-}
-
 sub suffix { return 'one' };
 
 1;
