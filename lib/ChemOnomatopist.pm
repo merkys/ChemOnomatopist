f26637ff162e734c6c1d54fd9a5f1fce6a6e6f08
Andrius Merkys <andrius.merkys@gmail.com>
2023-04-28 10:11:08 +0300

    Transfering the code from ChemOnomatopist::Group::Hydroxy::get_name() to ChemOnomatopist::select_main_chain().

--- lib/ChemOnomatopist.pm
+++ lib/ChemOnomatopist.pm
@@ -18,6 +18,7 @@ use ChemOnomatopist::Util::Graph qw(
     graph_center
     graph_has_cycle
     graph_longest_paths_from_vertex
+    graph_path_between_vertices
     tree_branch_positions
     tree_number_of_branches
 );
@@ -444,6 +445,59 @@ sub select_main_chain
     my @chains;
     if( $most_senior_group ) {
         # TODO: Select a chain containing most of the senior groups
+        my @groups = grep { blessed( $_ ) && $_->isa( $most_senior_group ) } $tree->vertices;
+        my @carbons = map { $_->C } @groups; # FIXME: Maybe uniq is needed?
+
+        if( @carbons == 1 ) {
+            # TODO: Select the best chain containing this one group
+        } else {
+            my @paths;
+            my $max_value;
+            for my $i (0..$#carbons) {
+                for my $j (($i+1)..$#carbons) {
+                    my @path = graph_path_between_vertices( $tree, $carbons[$i], $carbons[$j] );
+                    my $value = (set( @carbons ) * set( @path ))->size;
+                    if(      !defined $max_value || $max_value < $value ) {
+                        @paths = ( \@path );
+                        $max_value = $value;
+                    } elsif( $max_value == $value ) {
+                        push @paths, \@path;
+                    }
+                }
+            }
+
+            # Construct all chains having all possible extensions to both sides of the selected path
+            my %longest_paths;
+            for my $path (@paths) {
+                my $copy = $tree->copy;
+                $copy->delete_path( @$path );
+                $copy->delete_vertices( grep { !is_element( $_, 'C' ) } $copy->vertices );
+
+                my $A = shift @$path;
+                my $B = pop @$path;
+
+                if( !exists $longest_paths{$A} ) {
+                    $longest_paths{$A} = [ graph_longest_paths_from_vertex( $copy, $A ) ];
+                }
+                if( !exists $longest_paths{$B} ) {
+                    $longest_paths{$B} = [ graph_longest_paths_from_vertex( $copy, $B ) ];
+                }
+
+                for my $i (0..$#{$longest_paths{$A}}) {
+                    for my $j (0..$#{$longest_paths{$B}}) {
+                        push @chains,
+                             ChemOnomatopist::Chain::VertexArray->new( $tree,
+                                                                       reverse( @{$longest_paths{$A}->[$i]} ),
+                                                                       @$path,
+                                                                       @{$longest_paths{$B}->[$j]} ),
+                             ChemOnomatopist::Chain::VertexArray->new( $tree,
+                                                                       reverse( @{$longest_paths{$B}->[$j]} ),
+                                                                       reverse( @$path ),
+                                                                       @{$longest_paths{$A}->[$j]} );
+                    }
+                }
+            }
+        }
     } else {
         # Remove non-carbon atoms
         $tree = $tree->copy;
