b2763fc2c4ef43b89bbd520f75d86679e749a048
Andrius Merkys <andrius.merkys@gmail.com>
2023-05-16 15:55:57 +0300

    Adding a class for circular chains. Adding names from BBv2 P-22.2.1.

---
+++ lib/ChemOnomatopist/Chain/Circular.pm
@@ -0,0 +1,62 @@
+package ChemOnomatopist::Chain::Circular;
+
+use strict;
+use warnings;
+
+# ABSTRACT: Circular chain
+# VERSION
+
+use ChemOnomatopist::ChainHalf; # FIXME: Not sure why it is needed
+
+use parent ChemOnomatopist::ChainHalf::;
+
+our %names = (
+    OCNCC => '1,3-oxazolidine',
+    ONCCC => '1,2-oxazolidine',
+    NCCCC => 'pyrrolidine',
+    NNCCC => 'pyrazolidine',
+    NCNCC => 'imidazolidine',
+
+    NCCCCC => 'piperidine',
+    NCCNCC => 'piperazine',
+    OCCNCC => 'morpholine',
+
+    # 5-membered aromatic
+    'OC=CC=C'  => 'furan',
+    'NC=NC=C'  => '1H-imidazole', # FIXME: Adjust for isomerism
+    'OC=NC=C'  => '1,3-oxazole',
+    'ON=CC=C'  => '1,2-oxazole',
+    'NN=CC=C'  => '1H-pyrazole', # FIXME: Adjust for isomerism
+    'NC=CC=C'  => '1H-pyrole', # FIXME: Adjust for isomerism
+    'SeC=CC=C' => 'selenophene',
+    'TeC=CC=C' => 'tellurophene',
+    'SC=CC=C'  => 'thiophene',
+
+    # 6-membered aromatic
+    'C=CC=CC=C' => 'benzene',
+    'OCC=CC=C'  => '2H-pyran', # FIXME: Adjust for isomerism
+    'N=CC=NC=C' => 'pyrazine',
+    'N=NC=CC=C' => 'pyridazine',
+    'N=CC=CC=C' => 'pyridine',
+    'N=CN=CC=C' => 'pyrimidine',
+);
+
+sub new
+{
+    my( $class, $graph, @vertices ) = @_;
+    return bless { graph => $graph, vertices => \@vertices }, $class;
+}
+
+# sub branch_positions() # TODO: Maybe need to add 1 to all returned positions?
+
+sub _disconnected_chain_graph()
+{
+    my( $self ) = @_;
+
+    my $graph = $self->{graph}->copy;
+    $graph->delete_path( $self->vertices );
+
+    return $graph;
+}
+
+1;
