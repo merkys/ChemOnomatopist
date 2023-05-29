0cc83ec7b2793928b9b5cc68bdeabbfbdababc5a
Andrius Merkys <andrius.merkys@gmail.com>
2023-05-29 14:16:27 +0300

    Implementing is_homogeneous() check for circular chains.

--- lib/ChemOnomatopist/Chain/Circular.pm
+++ lib/ChemOnomatopist/Chain/Circular.pm
@@ -73,6 +73,21 @@ sub backbone_SMILES()
     return $SMILES;
 }
 
+sub is_homogeneous()
+{
+    my( $self ) = @_;
+
+    my @vertices = $self->vertices;
+    my @bonds = $self->bonds;
+
+    my $element = ucfirst $vertices[0]->{symbol};
+    my( $bond ) = @bonds;
+
+    return '' if any { ucfirst $_ ne $element } @vertices;
+    return '' if any { $_ ne $bond } @bonds;
+    return 1;
+}
+
 sub name()
 {
     my( $self ) = @_;
