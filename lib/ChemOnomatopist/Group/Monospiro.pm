4e173d4ac38da1376d92a4c517c47b2fc969ef14
Andrius Merkys <andrius.merkys@gmail.com>
2023-05-30 09:23:54 +0300

    Processing the monospiro compound in the constructor of ChemOnomatopist::Group::Monospiro.

--- lib/ChemOnomatopist/Group/Monospiro.pm
+++ lib/ChemOnomatopist/Group/Monospiro.pm
@@ -13,7 +13,13 @@ use ChemOnomatopist::Chain::Circular;
 sub new
 {
     my( $class, $graph, @vertices ) = @_;
-    return bless { graph => $graph, vertices => \@vertices }, $class;
+
+    my $subgraph = $graph->subgraph( \@vertices );
+    my( $spiro_atom ) = grep { $subgraph->degree( $_ ) == 4 } @vertices;
+    $subgraph->delete_vertex( $spiro_atom );
+    my @components = sort { scalar @$a <=> scalar @$b } $subgraph->connected_components;
+
+    return bless { graph => $graph, vertices => \@vertices, spiro_atom => $spiro_atom, components => \@components }, $class;
 }
 
 1;
