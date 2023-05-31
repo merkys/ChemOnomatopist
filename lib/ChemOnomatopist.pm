5f5b73b820fa2c658850e4bbd5338c4189092fd1
Andrius Merkys <andrius.merkys@gmail.com>
2023-05-31 12:14:50 +0300

    Linking Graph issue.

--- lib/ChemOnomatopist.pm
+++ lib/ChemOnomatopist.pm
@@ -403,7 +403,8 @@ sub find_groups
         }
     }
 
-    # Due to an issue in Graph, bridges() returns strings instead of real objects
+    # Due to the issue in Graph, bridges() returns strings instead of real objects.
+    # Graph issue: https://github.com/graphviz-perl/Graph/issues/29
     my %vertices_by_name = map { $_ => $_ } $graph->vertices;
     my $cut_vertices = set( map { $vertices_by_name{$_} } map { @$_ } $graph->bridges );
 
