b35687ded7c4bb017d1157e9fa57e5df3dec38ed
Andrius Merkys <andrius.merkys@gmail.com>
2023-06-27 15:24:44 +0300

    Bracketing names with substituent locants if they are to be prefixed with locants.

--- lib/ChemOnomatopist.pm
+++ lib/ChemOnomatopist.pm
@@ -300,6 +300,15 @@ sub get_mainchain_name
                   $attachment->has_substituent_locant ) ) {
                 $attachment->bracket;
             }
+        } else {
+            # This is an attempt to implement rules from P-16.5.1.
+            # However, they are quite vague, thus there is not much of guarantee the following code is correct.
+            if( $attachment !~ /^[\(\[\{]/ &&
+                $attachment->has_substituent_locant &&
+                $chain->needs_substituent_locants &&
+                $attachment ne 'tert-butyl' ) {
+                $attachment->bracket;
+            }
         }
 
         $name .= $attachment;
