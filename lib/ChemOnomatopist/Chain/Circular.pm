b4f71b4a1e6d6ee8d8563936132748bc3ad705a4
Andrius Merkys <andrius.merkys@gmail.com>
2023-07-04 15:41:11 +0300

    Adding stems to H-W names.

--- lib/ChemOnomatopist/Chain/Circular.pm
+++ lib/ChemOnomatopist/Chain/Circular.pm
@@ -152,7 +152,7 @@ sub name()
 
         if(      $self->length <= 5 ) {
             my @stems = ( 'ir', 'et', 'ol' );
-            $name .= $stems[$self->length - 3];
+            $name .= ChemOnomatopist::Name::Part::Stem->new( $stems[$self->length - 3] );
             if( $self->is_saturated ) {
                 $name .= $heteroatoms{N} ? 'idine' : 'ane';
             } elsif( $self->length == 3 ) {
@@ -174,7 +174,7 @@ sub name()
             return $name;
         } elsif( $self->length >= 7 ) {
             my @stems = ( 'ep', 'oc', 'on', 'ec' );
-            $name .= $stems[$self->length - 7];
+            $name .= ChemOnomatopist::Name::Part::Stem->new( $stems[$self->length - 7] );
             $name .= $self->is_saturated ? 'ane' : 'ine';
             return $name;
         }
