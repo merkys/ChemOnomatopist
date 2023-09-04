e4bb83b9d833d1a82a4f3fbb7213715393c4cc9f
Andrius Merkys <andrius.merkys@gmail.com>
2023-09-04 16:29:01 +0300

    Constructing suffixes for chalcogen analogues of hydroperoxides.

--- lib/ChemOnomatopist/Group/Hydroperoxide.pm
+++ lib/ChemOnomatopist/Group/Hydroperoxide.pm
@@ -6,6 +6,7 @@ use warnings;
 # ABSTRACT: Hydroperoxide group
 # VERSION
 
+use ChemOnomatopist::Elements qw( %elements );
 use List::Util qw( all );
 
 use parent ChemOnomatopist::Group::;
@@ -24,7 +25,17 @@ sub suffix
 {
     my( $self ) = @_;
     return 'peroxol' if all { ChemOnomatopist::element( $_ ) eq 'O' } @{$self->{atoms}};
-    return '-' . join( '', map { $_->{symbol} } @{$self->{atoms}} ) . '-peroxol'; # FIXME: Missing element names
+
+    my @elements = map { ChemOnomatopist::element( $_ ) } @{$self->{atoms}};
+    my $name;
+    if( $elements[0] eq $elements[1] ) {
+        $name = 'di' . $elements{$elements[0]}->{prefix};
+        $name =~ s/a$/o/;
+    } else {
+        $name = '-' . join( '', @elements ) . '-' .
+                join '', sort map { s/a$/o/; $_ } map { $elements{$_}->{prefix} } grep { $_ ne 'O' } @elements;
+    }
+    return $name . 'peroxol';
 }
 
 1;
