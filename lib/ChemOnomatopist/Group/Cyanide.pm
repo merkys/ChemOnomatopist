2d65be27c3ac126c4e3978d1b5203c9720c3afa4
Andrius Merkys <andrius.merkys@gmail.com>
2023-09-05 11:24:13 +0300

    Adding a FIXME note for ChemOnomatopist::Group::Cyanide.

--- lib/ChemOnomatopist/Group/Cyanide.pm
+++ lib/ChemOnomatopist/Group/Cyanide.pm
@@ -8,6 +8,8 @@ use warnings;
 
 use parent ChemOnomatopist::Group::;
 
+# FIXME: When part of chain, suffix is 'nitrile'; when an attachment - 'carbonitrile'.
+
 sub prefix { return 'cyano' }
 sub suffix { return 'carbonitrile' }
 
