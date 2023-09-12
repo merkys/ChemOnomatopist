3c0d1faa25868cc7aabd8b5d05e5cb0af2778b48
Andrius Merkys <andrius.merkys@gmail.com>
2023-09-12 13:58:58 +0300

    ChemOnomatopist::Group::Polyacene::ideal_graph() now can construct naphthalene too.

--- lib/ChemOnomatopist/Group/Polyacene.pm
+++ lib/ChemOnomatopist/Group/Polyacene.pm
@@ -73,7 +73,7 @@ sub locants(@)
 sub ideal_graph($$)
 {
     my( $class, $N ) = @_;
-    die "cannot construct polyacene with $N vertices\n" if $N < 18 || ($N-2) % 4;
+    die "cannot construct polyacene with $N vertices\n" if $N < 10 || ($N-2) % 4;
 
     my $graph = Graph::Undirected->new( refvertexed => 1 );
     my @vertices = map { { symbol => 'C', number => $_-1 } } 1..$N;
