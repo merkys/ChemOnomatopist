e23bd79eb66e856a849db52437b48b5e0625d1d6
Andrius Merkys <andrius.merkys@gmail.com>
2023-10-16 11:46:54 +0300

    Making amide group a chain.

--- lib/ChemOnomatopist/Group/Amide.pm
+++ lib/ChemOnomatopist/Group/Amide.pm
@@ -6,17 +6,20 @@ use warnings;
 # ABSTRACT: Amide group
 # VERSION
 
-use parent ChemOnomatopist::Group::;
+use parent ChemOnomatopist::Group::, ChemOnomatopist::Chain::;
 
 sub new
 {
-    my( $class, $parent ) = @_;
-    return bless { parent => $parent }, $class;
+    my( $class, $graph, $amide, $ketone, $C ) = @_;
+    return bless { graph => $graph, amide => $amide, ketone => $ketone, C => $C }, $class;
 }
 
-sub element() { return 'N' }
-
-sub is_terminal() { return 1 }
+sub vertices
+{
+    my( $self ) = @_;
+    my @vertices = ( $self->{amide}, $self->{ketone}, $self->{C} );
+    return @vertices;
+}
 
 sub prefix { return 'amido' }
 sub suffix { return 'amide' }
