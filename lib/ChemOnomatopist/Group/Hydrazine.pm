638ca129a89beff6d98866f2aef196c3903440d0
Andrius Merkys <andrius.merkys@gmail.com>
2024-02-09 15:49:23 +0200

    Cleaning up diazene handling code from ChemOnomatopist::Group::Hydrazine.

--- lib/ChemOnomatopist/Group/Hydrazine.pm
+++ lib/ChemOnomatopist/Group/Hydrazine.pm
@@ -8,8 +8,6 @@ use warnings;
 
 use parent ChemOnomatopist::Group::, ChemOnomatopist::Chain::;
 
-use Chemistry::OpenSMILES qw( is_double_bond );
-
 sub new
 {
     my( $class, $graph, @vertices ) = @_;
@@ -34,27 +32,7 @@ sub needs_substituent_locants()
     return $self->number_of_branches > 1 && $self->number_of_branches < $self->max_valence;
 }
 
-# Twice-substituted diazenes do not need suffix locant
-sub needs_suffix_locant() { $_[0]->max_valence != 2 || $_[0]->number_of_branches != 2 }
-
-sub prefix()
-{
-    my( $self ) = @_;
-    if( is_double_bond( $self->{graph}, $self->vertices ) ) {
-        return 'diazenyl';
-    } else {
-        return 'hydrazinyl';
-    }
-}
-
-sub suffix()
-{
-    my( $self ) = @_;
-    if( is_double_bond( $self->{graph}, $self->vertices ) ) {
-        return 'diazene';
-    } else {
-        return 'hydrazine';
-    }
-}
+sub prefix() { 'hydrazinyl' }
+sub suffix() { 'hydrazine' }
 
 1;
