b0e2697f567b16b39db39d8dfdafc357e5027a41
Andrius Merkys <andrius.merkys@gmail.com>
2024-02-09 11:10:50 +0200

    Memorize the ketone element in ChemOnomatopist::Group::Urea.

--- lib/ChemOnomatopist/Group/Urea.pm
+++ lib/ChemOnomatopist/Group/Urea.pm
@@ -15,7 +15,13 @@ use parent ChemOnomatopist::Group::, ChemOnomatopist::Chain::;
 sub new
 {
     my( $class, $graph, @vertices ) = @_;
-    return bless { graph => $graph, vertices => \@vertices }, $class;
+    my( $ketone_element ) = map  { $_->{ketone}->element }
+                            grep { blessed $_ && $_->isa( ChemOnomatopist::Group::Amide:: ) }
+                                 @vertices;
+    return bless { graph => $graph,
+                   vertices => \@vertices,
+                   ketone_element => $ketone_element },
+                 $class;
 }
 
 sub needs_substituent_locants() { $_[0]->number_of_branches > 1 && $_[0]->number_of_branches < 4 }
@@ -31,12 +37,9 @@ sub prefix() { 'carbamoylamino' }
 sub suffix()
 {
     my( $self ) = @_;
-    my( $ketone_element ) = map  { $_->{ketone}->element }
-                            grep { blessed $_ && $_->isa( ChemOnomatopist::Group::Amide:: ) }
-                                 $self->vertices;
     my $name = '';
-    if( $ketone_element ne 'O' ) {
-        $name = $elements{$ketone_element}->{prefix};
+    if( $self->{ketone_element} ne 'O' ) {
+        $name = $elements{$self->{ketone_element}}->{prefix};
         $name =~ s/a$/o/;
     }
     return $name . 'urea';
