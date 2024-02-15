6d6f50db19c29c279f4de1cca9b66ab83b641678
Andrius Merkys <andrius.merkys@gmail.com>
2024-02-15 12:08:04 +0200

    Minor syntax adjustment in ChemOnomatopist::Group::Carboxyl.

--- lib/ChemOnomatopist/Group/Carboxyl.pm
+++ lib/ChemOnomatopist/Group/Carboxyl.pm
@@ -1,18 +1,18 @@
 package ChemOnomatopist::Group::Carboxyl;
 
-use strict;
-use warnings;
-
 # ABSTRACT: Carboxyl group
 # VERSION
 
+use strict;
+use warnings;
+
 use parent ChemOnomatopist::Group::;
 
-sub element() { return 'C' }
+sub element() { 'C' }
 
-sub prefix() { return 'carboxy' }
-sub suffix() { return 'oic acid' } # FIXME: Should be 'carboxylic acid' if attached to cycles
-sub multisuffix() { return 'carboxylic acid' }
-sub suffix_if_cycle_substituent() { return $_[0]->multisuffix }
+sub prefix() { 'carboxy' }
+sub suffix() { 'oic acid' } # FIXME: Should be 'carboxylic acid' if attached to cycles
+sub multisuffix() { 'carboxylic acid' }
+sub suffix_if_cycle_substituent() { $_[0]->multisuffix }
 
 1;
