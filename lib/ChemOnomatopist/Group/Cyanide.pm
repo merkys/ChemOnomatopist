e8b01d2470bf7a6eb4c631d2e21908c3746b9bf0
Andrius Merkys <andrius.merkys@gmail.com>
2024-02-19 15:54:29 +0200

    Add ChemOnomatopist::Group::Cyanide::multisuffix().

--- lib/ChemOnomatopist/Group/Cyanide.pm
+++ lib/ChemOnomatopist/Group/Cyanide.pm
@@ -1,14 +1,15 @@
 package ChemOnomatopist::Group::Cyanide;
 
-use strict;
-use warnings;
-
 # ABSTRACT: Cyanide group
 # VERSION
 
+use strict;
+use warnings;
+
 use parent ChemOnomatopist::Group::;
 
-sub prefix { return 'cyano' }
-sub suffix { return 'nitrile' }
+sub prefix() { 'cyano' }
+sub suffix() { 'nitrile' }
+sub multisuffix() { 'carbonitrile' }
 
 1;
