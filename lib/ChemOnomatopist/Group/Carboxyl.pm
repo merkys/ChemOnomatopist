431139397d94c9b1012d92029abadeca929569e5
Andrius Merkys <andrius.merkys@gmail.com>
2024-02-19 13:04:06 +0200

    Attempting to understand when element name is added before the word 'acid' in carboxyl group names.

--- lib/ChemOnomatopist/Group/Carboxyl.pm
+++ lib/ChemOnomatopist/Group/Carboxyl.pm
@@ -29,11 +29,12 @@ sub suffix()
     return ChemOnomatopist::Name->new( 'oic acid' ) if $ketone->element eq 'O';
 
     my $element_prefix = $elements{$ketone->element}->{prefix};
-    $element_prefix =~ s/a$/oic acid/;
+    $element_prefix =~ s/a$/oic /;
 
     my $name = ChemOnomatopist::Name->new;
     $name .= ChemOnomatopist::Name::Part::Multiplier->new( 'di' ) if $hydroxy->element eq $ketone->element;
     $name .= $element_prefix;
+    $name .= 'acid';
     return $name;
 }
 
@@ -45,11 +46,12 @@ sub multisuffix()
     return ChemOnomatopist::Name->new( 'carboxylic acid' ) if $ketone->element eq 'O';
 
     my $element_prefix = $elements{$ketone->element}->{prefix};
-    $element_prefix =~ s/a$/oic acid/;
+    $element_prefix =~ s/a$/oic /;
 
     my $name = ChemOnomatopist::Name->new( 'carbo' );
     $name .= ChemOnomatopist::Name::Part::Multiplier->new( 'di' ) if $hydroxy->element eq $ketone->element;
     $name .= $element_prefix;
+    $name .= 'acid';
     return $name;
 }
 
