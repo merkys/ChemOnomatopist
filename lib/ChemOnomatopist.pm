b73feb8473541c7b82bfd2e0fe44659228752acd
Andrius Merkys <andrius.merkys@gmail.com>
2024-04-11 15:26:29 +0300

    Fixing naming of acetylene.

--- lib/ChemOnomatopist.pm
+++ lib/ChemOnomatopist.pm
@@ -486,6 +486,7 @@ sub get_mainchain_name
     $name =~ s/benzen(-1-)?ol$/phenol/;
     $name = 'anisole'      if $name eq 'methoxybenzene';         # BBv2 P-63.2.4.1
     $name = 'benzoic acid' if $name eq 'benzenecarboxylic acid'; # BBv2 P-65.1.1.1
+    $name = 'acetylene'    if $name eq 'ethyne';                 # BBv3 P-52.2.1.1
     $name = 'toluene'      if $name eq 'methylbenzene';
     $name =~ s/^(\d,\d-)dimethylbenzene$/$1xylene/;
     $name = 'formic acid' if $name eq 'methanoic acid';
