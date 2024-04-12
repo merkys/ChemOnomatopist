ce9bca5fcae0b1ed2d892f316a5b1be2ab1ade14
Andrius Merkys <andrius.merkys@gmail.com>
2024-04-12 12:30:14 +0300

    Reduce dependence on is_carbon().

--- lib/ChemOnomatopist/Chain.pm
+++ lib/ChemOnomatopist/Chain.pm
@@ -353,7 +353,9 @@ sub needs_suffix_locant()
     my @most_senior_groups = $self->most_senior_groups;
     return '' unless @most_senior_groups;
     return 1 if $self->number_of_heteroatoms; # P-15.4.3.2.3: Characteristic groups cited as suffixes are given locants
-    return 1 if !$most_senior_groups[0]->is_carbon || @most_senior_groups > 2;
+    return 1 if @most_senior_groups > 2;
+    return 1 if !ChemOnomatopist::element( $most_senior_groups[0] );
+    return 1 if  ChemOnomatopist::element( $most_senior_groups[0] ) ne 'C';
     return '';
 }
 
