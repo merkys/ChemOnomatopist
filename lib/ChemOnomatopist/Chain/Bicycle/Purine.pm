5f6721a8dacb48f89f1b187e940f177ef3b28341
Andrius Merkys <andrius.merkys@gmail.com>
2024-04-16 18:15:32 +0300

    Fixing pyrimidine constructor.

--- lib/ChemOnomatopist/Chain/Bicycle/Purine.pm
+++ lib/ChemOnomatopist/Chain/Bicycle/Purine.pm
@@ -6,23 +6,30 @@ package ChemOnomatopist::Chain::Bicycle::Purine;
 use strict;
 use warnings;
 
+use Graph::Traversal::DFS;
+use List::Util qw( first );
+
 use parent ChemOnomatopist::Chain::Bicycle::;
 
 sub new
 {
-    my( $class, $graph, $pyrimidine, $imidazole ) = @_;
+    my( $class, $graph, @cycles ) = @_;
 
+    my $pyrimidine = first { $_->length == 6 } @cycles;
     if( ChemOnomatopist::element( $pyrimidine->{vertices}[0] ) eq 'N' ) {
-        ( $pyrimidine, $imidazole ) = map { $_->flipped }
-                                          ( $pyrimidine, $imidazole );
+        $pyrimidine = $pyrimidine->flipped;
     }
 
-    my @vertices = ( @{$pyrimidine->{vertices}}[1..5],
-                     $pyrimidine->{vertices}[0],
-                     reverse @{$imidazole->{vertices}}[0..2] );
+    my $subgraph = $graph->subgraph( map { $_->vertices } @cycles );
+    $subgraph->delete_edge( @{$pyrimidine->{vertices}}[0..1] );
+    $subgraph->delete_edge( map  { @$_ }
+                            grep { $subgraph->degree( $_->[0] ) == 3 &&
+                                   $subgraph->degree( $_->[1] ) == 3 }
+                                 $subgraph->edges );
+    my @vertices = Graph::Traversal::DFS->new( $subgraph, start => $pyrimidine->{vertices}[0] )->dfs;
     return bless { graph => $graph,
-                   cycles => [ $pyrimidine, $imidazole ],
-                   vertices => \@vertices };
+                   cycles => \@cycles,
+                   vertices => \@vertices }, $class;
 }
 
 sub is_purine() { 1 }
