3c3b1babaebc5a0c8d5d9cb8d8939f1057f89812
Andrius Merkys <andrius.merkys@gmail.com>
2024-05-07 13:25:47 +0300

    Drop excess space.

--- lib/ChemOnomatopist.pm
+++ lib/ChemOnomatopist.pm
@@ -372,7 +372,6 @@ sub get_mainchain_name
         }
     }
 
-
     # Collecting names of all the attachments
     my @order = sort { cmp_only_alphabetical( $a, $b ) || $a cmp $b } keys %attachments;
     my $name = ChemOnomatopist::Name->new;
