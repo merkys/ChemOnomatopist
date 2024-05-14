4dc067542350c07b18b6c33ab476322958dae817
Andrius Merkys <andrius.merkys@gmail.com>
2024-05-14 15:50:53 +0300

    Paying attention to the elements at hydroxy group when naming sulfonic acids.

--- lib/ChemOnomatopist/Group/SulfonicAcid.pm
+++ lib/ChemOnomatopist/Group/SulfonicAcid.pm
@@ -9,11 +9,28 @@ use warnings;
 use parent ChemOnomatopist::Group::;
 
 use ChemOnomatopist::Name;
+use List::Util qw( first );
+
+sub new
+{
+    my( $class, @attachments ) = @_;
+    return bless { attachments => \@attachments }, $class;
+}
 
 sub element() { 'S' }
 
-# From BBv2 P-65.3.0
+# From BBv2 P-65.3.0 and Table 4.3
 sub prefix() { ChemOnomatopist::Name->new( 'sulfo' ) }
-sub suffix() { ChemOnomatopist::Name->new( 'sulfonic acid' ) }
+sub suffix()
+{
+    my( $self ) = @_;
+
+    my $hydroxy = first { $_->isa( ChemOnomatopist::Group::Hydroxy:: ) } @{$self->{attachments}};
+    if( $hydroxy->element eq 'S' ) {
+        return ChemOnomatopist::Name->new( 'sulfonothioic S-acid' );
+    } else {
+        return ChemOnomatopist::Name->new( 'sulfonic acid' );
+    }
+}
 
 1;
