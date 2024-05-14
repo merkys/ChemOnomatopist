c3feb8297a76b1dc04e0e285cdc447eb1ec26629
Andrius Merkys <andrius.merkys@gmail.com>
2024-05-14 15:36:31 +0300

    Update syntax of ChemOnomatopist::Group::SulfonicAcid.

--- lib/ChemOnomatopist/Group/SulfonicAcid.pm
+++ lib/ChemOnomatopist/Group/SulfonicAcid.pm
@@ -1,17 +1,19 @@
 package ChemOnomatopist::Group::SulfonicAcid;
 
-use strict;
-use warnings;
-
 # ABSTRACT: Sulfonic acid group
 # VERSION
 
+use strict;
+use warnings;
+
 use parent ChemOnomatopist::Group::;
 
-sub element() { return 'S' }
+use ChemOnomatopist::Name;
+
+sub element() { 'S' }
 
 # From BBv2 P-65.3.0
-sub prefix() { return 'sulfo' }
-sub suffix() { return 'sulfonic acid' }
+sub prefix() { ChemOnomatopist::Name->new( 'sulfo' ) }
+sub suffix() { ChemOnomatopist::Name->new( 'sulfonic acid' ) }
 
 1;
