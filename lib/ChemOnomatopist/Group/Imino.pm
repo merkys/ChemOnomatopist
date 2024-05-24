7fc9625efbd89dd89e73571fb966e25a2d8650c0
Andrius Merkys <andrius.merkys@gmail.com>
2024-05-24 10:08:40 +0300

    Return name objects from ChemOnomatopist::Group::Imino.

--- lib/ChemOnomatopist/Group/Imino.pm
+++ lib/ChemOnomatopist/Group/Imino.pm
@@ -8,13 +8,15 @@ use warnings;
 
 use parent ChemOnomatopist::Group::;
 
+use ChemOnomatopist::Name;
+
 sub element() { 'N' }
 
 sub is_terminal() { 1 }
 
 sub needs_multiple_bond_suffix() { '' }
 
-sub prefix() { 'imino' }
-sub suffix() { 'imine' }
+sub prefix() { ChemOnomatopist::Name->new( 'imino' ) }
+sub suffix() { ChemOnomatopist::Name->new( 'imine' ) }
 
 1;
