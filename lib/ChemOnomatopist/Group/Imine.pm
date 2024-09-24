7179edaaeffe997daf52349908f78d1ae0a21d2a
Andrius Merkys <andrius.merkys@gmail.com>
2024-09-24 11:24:28 +0300

    Work to support charges in imine groups.

--- lib/ChemOnomatopist/Group/Imine.pm
+++ lib/ChemOnomatopist/Group/Imine.pm
@@ -10,8 +10,14 @@ use parent ChemOnomatopist::Group::;
 
 use ChemOnomatopist::Name;
 
-sub element() { 'N' }
+sub new
+{
+    my( $class, $atom ) = @_;
+    return bless { atom => $atom }, $class;
+}
 
+sub element() { 'N' }
+sub charge() { ChemOnomatopist::charge( $_[0]->{atom} ) }
 sub is_terminal() { 1 }
 
 sub needs_multiple_bond_suffix() { '' }
