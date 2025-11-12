5d28383b882b88f0e79e28fb8a91e032f26175a8
Andrius Merkys <andrius.merkys@gmail.com>
2025-11-12 10:38:25 +0200

    Get around an uninitialised value warning.

--- lib/ChemOnomatopist/Group/Amine.pm
+++ lib/ChemOnomatopist/Group/Amine.pm
@@ -21,6 +21,6 @@ sub charge() { ChemOnomatopist::charge( $_[0]->{atom} ) }
 sub is_terminal() { 1 }
 
 sub prefix() { ChemOnomatopist::Name->new( 'amino' ) }
-sub suffix() { ChemOnomatopist::Name->new( $_[0]->charge == -1 ? 'aminide' : 'amine' ) }
+sub suffix() { ChemOnomatopist::Name->new( $_[0]->charge && $_[0]->charge == -1 ? 'aminide' : 'amine' ) }
 
 1;
